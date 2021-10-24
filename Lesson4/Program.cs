4f630ec96bb1bca1065591525d65f0fa52ba47c2
akolygo <akolygo@gmail.com>
2021-10-24 14:58:38 +0300

    Lesson4

---
+++ Lesson4/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace Buildings
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("I'm too lazy to do 4.2 and 4.3");
+            var a = new Buildings(2,2);
+            a.PrintDetalis();
+        }
+    }
+}
